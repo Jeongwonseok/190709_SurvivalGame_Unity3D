124aaf647bff5c5e06639219f499e2042a17f700
“Jeongwonseok” <jws4281@nate.com>
2019-07-11 14:49:45 +0900

    Mining
    광석 채굴 구현

--- Assets/Scripts/PickaxeController.cs
+++ Assets/Scripts/PickaxeController.cs
@@ -26,6 +26,10 @@ public class PickaxeController : CloseWeaponController
         {
             if (CheckObject())
             {
+                if(hitInfo.transform.tag == "Rock")
+                {
+                    hitInfo.transform.GetComponent<Rock>().Mining();
+                }
                 isSwing = false;
                 Debug.Log(hitInfo.transform.name);
             }
---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    // 바위의 체력
+    [SerializeField]
+    private int hp;
+
+    // 파편 제거시간
+    [SerializeField]
+    private float destroyTime;
+
+    // 구체 콜라이더 
+    [SerializeField]
+    private SphereCollider col;
+
+    // 필요한 게임 오브젝트
+    // 일반 바위
+    [SerializeField]
+    private GameObject go_rock;
+    // 깨진 바위
+    [SerializeField]
+    private GameObject go_debris;
+    // 채굴 이펙트
+    [SerializeField]
+    private GameObject go_effect_prefabs;
+
+    // 오디오
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip effect_sound;
+    [SerializeField]
+    private AudioClip effect_sound2;
+
+    public void Mining()
+    {
+        audioSource.clip = effect_sound;
+        audioSource.Play();
+        var clone = Instantiate(go_effect_prefabs, col.bounds.center, Quaternion.identity);
+        Destroy(clone, destroyTime);
+
+        hp--;
+        if(hp<=0)
+        {
+            Destruction();
+        }
+    }
+
+    private void Destruction()
+    {
+        audioSource.clip = effect_sound2;
+        audioSource.Play();
+
+        col.enabled = false;
+        Destroy(go_rock); // 바로 삭제
+
+        go_debris.SetActive(true);
+        Destroy(go_debris, destroyTime); // 일정시간 간격 주고 삭제
+    }
+}
