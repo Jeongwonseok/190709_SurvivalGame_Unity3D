222b8250a1fc99b327f07b475f5c822026660def
“Jeongwonseok” <jws4281@nate.com>
2019-07-23 22:24:56 +0900

    Build System
    모닥불 건축 및 건축 불가 지역 구현

---
+++ Assets/Scripts/PreviewObject.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewObject : MonoBehaviour
+{
+    // 충돌한 오브젝트의 컬라이더 저장하는 리스트
+    private List<Collider> colliderList = new List<Collider>();
+
+    [SerializeField]
+    private int layerGround; // 지상 레이어
+    private const int IGNORE_RAYCAST_LAYER=2;
+
+    [SerializeField]
+    private Material green;
+    [SerializeField]
+    private Material red;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeColor();
+    }
+
+    private void ChangeColor()
+    {
+        if (colliderList.Count > 0)
+            SetColor(red);
+        else
+            SetColor(green);
+
+
+    }
+
+    private void SetColor(Material _mat)
+    {
+        foreach(Transform tf_Child in this.transform)
+        {
+            var newMaterials = new Material[tf_Child.GetComponent<Renderer>().materials.Length];
+
+            for (int i = 0; i < newMaterials.Length; i++)
+            {
+                newMaterials[i] = _mat;
+            }
+
+            tf_Child.GetComponent<Renderer>().materials = newMaterials;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer != layerGround && other.gameObject.layer != IGNORE_RAYCAST_LAYER)
+        {
+            colliderList.Add(other);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer != layerGround && other.gameObject.layer != IGNORE_RAYCAST_LAYER)
+        {
+            colliderList.Remove(other);
+        }
+    }
+
+    public bool isBuildable()
+    {
+        return colliderList.Count == 0;
+    }
+}
---
+++ Assets/Scripts/UI Script/CraftManual.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Craft
+{
+    public string craftName; // 이름
+    public GameObject go_Prefab; // 실제 설치될 프리팹
+    public GameObject go_PreviewPrefab; // 미리보기 프리팹
+
+
+}
+
+public class CraftManual : MonoBehaviour
+{
+    // 상태변수
+    private bool isActivated = false;
+    private bool isPreviewActivated = false;
+
+    [SerializeField]
+    private GameObject go_BaseUI; // 기본 베이스 UI
+
+    [SerializeField]
+    private Craft[] craft_fire; // 모닥불용 탭
+
+    private GameObject go_Preview; // 미리보기 프리팹을 담을 변수
+    private GameObject go_Prefab; // 실제 생성될 프리팹을 담을 변수
+
+    [SerializeField]
+    private Transform tf_Player; // 플레이어 위치
+
+    // Raycast 필요 변수 선언
+    private RaycastHit hitInfo;
+    [SerializeField]
+    private LayerMask layerMask;
+    [SerializeField]
+    private float range;
+
+    public void SlotClick(int _slotNumber)
+    {
+        go_Preview = Instantiate(craft_fire[_slotNumber].go_PreviewPrefab, tf_Player.position + tf_Player.forward, Quaternion.identity);
+        go_Prefab = craft_fire[_slotNumber].go_Prefab;
+        isPreviewActivated = true;
+        go_BaseUI.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab) && !isPreviewActivated)
+        {
+            Window();
+        }
+
+        if(isPreviewActivated)
+        {
+            PreviewPositionUpdate();
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Build();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cancel();
+        }
+    }
+
+    private void Build()
+    {
+        if(isPreviewActivated && go_Preview.GetComponent<PreviewObject>().isBuildable())
+        {
+            Destroy(go_Preview);
+            Instantiate(go_Prefab, hitInfo.point, Quaternion.identity);
+            isActivated = false;
+            isPreviewActivated = false;
+            go_Preview = null;
+            go_Prefab = null;
+        }
+    }
+
+    private void PreviewPositionUpdate()
+    {
+        if(Physics.Raycast(tf_Player.position, tf_Player.forward, out hitInfo, range, layerMask))
+        {
+            if(hitInfo.transform != null)
+            {
+                Vector3 _location = hitInfo.point;
+                go_Preview.transform.position = _location;
+            }
+        }
+    }
+
+    private void Cancel()
+    {
+        if(isPreviewActivated)
+            Destroy(go_Preview);
+
+        isActivated = false;
+        isPreviewActivated = false;
+        go_Preview = null;
+        go_Prefab = null;
+
+        go_BaseUI.SetActive(false);
+    }
+
+    private void Window()
+    {
+        if(!isActivated)
+            OpenWindow();
+        else
+            CloseWindow();
+    }
+
+    private void OpenWindow()
+    {
+        isActivated = true;
+        go_BaseUI.SetActive(true);
+    }
+
+    private void CloseWindow()
+    {
+        isActivated = false;
+        go_BaseUI.SetActive(false);
+    }
+}
