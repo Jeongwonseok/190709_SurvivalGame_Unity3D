fbf929fb21388a7d54ae3e1fcaf64344a4b34425
“Jeongwonseok” <jws4281@nate.com>
2019-07-26 21:31:42 +0900

    Title Scene
    타이틀 Scene 구현

---
+++ Assets/Scripts/UI Script/Title.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+
+    public string sceneName= "GameStage";
+
+    public void ClickStart()
+    {
+        Debug.Log("로딩");
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ClickLoad()
+    {
+        Debug.Log("로드");
+    }
+
+    public void ClickExit()
+    {
+        Debug.Log("게임 종료");
+        Application.Quit();
+    }
+}
