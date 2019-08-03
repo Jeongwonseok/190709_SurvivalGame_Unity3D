1d52d4335a2df48f21e568524a28531c50f28aaf
“Jeongwonseok” <jws4281@nate.com>
2019-08-03 20:39:04 +0900

    오류 수정3
    오류수정

---
+++ Assets/Scripts/Water/SaveNLoad.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class SaveData
+{
+    public Vector3 playerPos;
+    public Vector3 playerRot;
+
+    public List<int> invenArrayNum = new List<int>();
+    public List<string> invenItemName = new List<string>();
+    public List<int> invenItemNum = new List<int>();
+
+}
+
+public class SaveNLoad : MonoBehaviour
+{
+    private SaveData saveData = new SaveData();
+
+    private string SAVE_DATA_DIRECTORY;
+    private string SAVE_FILENAME = "/SaveFile.txt";
+
+    private PlayerController thePlayer;
+    private Inventory theInven;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SAVE_DATA_DIRECTORY = Application.dataPath + "/Saves/";
+
+        if (!Directory.Exists(SAVE_DATA_DIRECTORY))
+        {
+            Directory.CreateDirectory(SAVE_DATA_DIRECTORY);
+        }
+    }
+
+    public void SaveData()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
+        theInven = FindObjectOfType<Inventory>();
+
+        saveData.playerPos = thePlayer.transform.position;
+        saveData.playerRot = thePlayer.transform.eulerAngles;
+
+        Slot[] slots = theInven.GetSlots();
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (slots[i].item != null)
+            {
+                saveData.invenArrayNum.Add(i);
+                saveData.invenItemName.Add(slots[i].item.itemName);
+                saveData.invenItemNum.Add(slots[i].itemCount);
+            }
+        }
+
+        string json = JsonUtility.ToJson(saveData);
+
+        File.WriteAllText(SAVE_DATA_DIRECTORY + SAVE_FILENAME, json);
+
+        Debug.Log("저장 완료");
+        Debug.Log(json);
+    }
+
+    public void LoadData()
+    {
+        if (File.Exists(SAVE_DATA_DIRECTORY + SAVE_FILENAME))
+        {
+            string loadJson = File.ReadAllText(SAVE_DATA_DIRECTORY + SAVE_FILENAME);
+            saveData = JsonUtility.FromJson<SaveData>(loadJson);
+
+            thePlayer = FindObjectOfType<PlayerController>();
+            theInven = FindObjectOfType<Inventory>();
+
+            thePlayer.transform.position = saveData.playerPos;
+            thePlayer.transform.eulerAngles = saveData.playerRot;
+
+            for (int i = 0; i < saveData.invenItemNum.Count; i++)
+            {
+                theInven.LoadToInven(saveData.invenArrayNum[i], saveData.invenItemName[i], saveData.invenItemNum[i]);
+            }
+
+            Debug.Log("로드 완료");
+        }
+        else
+            Debug.Log("세이브 파일이 없습니다.");
+    }
+}
