dd76660b734837ddac35c5b2fbb3c58e2aada7ef
“Jeongwonseok” <jws4281@nate.com>
2019-08-22 20:35:28 +0900

    CraftManual_5
    건축UI 탭과 페이지 시스템
    - 불, 건축 탭 구분
    - 각 탭마다 페이지 이동 가능(좌우)
    - 요소 선택 시 맞는 건축물 구현

--- Assets/Scripts/UI Script/CraftManual.cs
+++ Assets/Scripts/UI Script/CraftManual.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class Craft
 {
     public string craftName; // 이름
+    public Sprite craftImage; // 이미지
+    public string craftDesc; // 설명
     public GameObject go_Prefab; // 실제 설치될 프리팹
     public GameObject go_PreviewPrefab; // 미리보기 프리팹
 
@@ -21,8 +24,15 @@ public class CraftManual : MonoBehaviour
     [SerializeField]
     private GameObject go_BaseUI; // 기본 베이스 UI
 
+    private int tabNumber = 0;
+    private int page = 1;
+    private int selectedSlotNumber;
+    private Craft[] craft_SelectedTab;
+
     [SerializeField]
     private Craft[] craft_fire; // 모닥불용 탭
+    [SerializeField]
+    private Craft[] craft_build; // 건축용 탭
 
     private GameObject go_Preview; // 미리보기 프리팹을 담을 변수
     private GameObject go_Prefab; // 실제 생성될 프리팹을 담을 변수
@@ -37,10 +47,99 @@ public class CraftManual : MonoBehaviour
     [SerializeField]
     private float range;
 
+    // 필요한 UI Slot 요소
+    [SerializeField]
+    private GameObject[] go_Slots;
+    [SerializeField]
+    private Image[] image_Slot;
+    [SerializeField]
+    private Text[] text_SlotName;
+    [SerializeField]
+    private Text[] text_SlotDesc;
+
+    void Start()
+    {
+        tabNumber = 0;
+        page = 1;
+        TabSlotSetting(craft_fire);
+
+    }
+
+    public void TabSetting(int _tabNumber)
+    {
+        tabNumber = _tabNumber;
+        page = 1;
+
+        switch(tabNumber)
+        {
+            case 0:
+                TabSlotSetting(craft_fire); // 불 세팅
+                break;
+            case 1:
+                TabSlotSetting(craft_build); // 건축 세팅
+                break;
+        }
+    }
+
+    private void ClearSlot()
+    {
+        for (int i = 0; i < go_Slots.Length; i++)
+        {
+            image_Slot[i].sprite = null;
+            text_SlotName[i].text = "";
+            text_SlotDesc[i].text = "";
+            go_Slots[i].SetActive(false);
+        }
+    }
+
+    public void RightPageSetting()
+    {
+        if (page < (craft_SelectedTab.Length / go_Slots.Length) + 1)
+            page++;
+        else
+            page = 1;
+
+        TabSlotSetting(craft_SelectedTab);
+    }
+
+    public void LeftPageSetting()
+    {
+        if (page != 1)
+            page--;
+        else
+            page = (craft_SelectedTab.Length / go_Slots.Length) + 1;
+
+        TabSlotSetting(craft_SelectedTab);
+    }
+
+    private void TabSlotSetting(Craft[] _craft_tab)
+    {
+        // 일단 초기화
+        ClearSlot();
+
+        craft_SelectedTab = _craft_tab;
+
+        int startSlotNumber = (page - 1) * go_Slots.Length; // 4의 배수 >> 슬롯 개수가 4개이기 때문에..
+
+        for (int i = startSlotNumber; i < craft_SelectedTab.Length; i++)
+        {
+            if (i == page * go_Slots.Length)
+                break;
+
+            go_Slots[i - startSlotNumber].SetActive(true);
+
+            image_Slot[i - startSlotNumber].sprite = craft_SelectedTab[i].craftImage;
+            text_SlotName[i - startSlotNumber].text = craft_SelectedTab[i].craftName;
+            text_SlotDesc[i - startSlotNumber].text = craft_SelectedTab[i].craftDesc;
+        }
+    }
+
     public void SlotClick(int _slotNumber)
     {
-        go_Preview = Instantiate(craft_fire[_slotNumber].go_PreviewPrefab, tf_Player.position + tf_Player.forward, Quaternion.identity);
-        go_Prefab = craft_fire[_slotNumber].go_Prefab;
+        selectedSlotNumber = _slotNumber + (page - 1) * go_Slots.Length;
+
+        go_Preview = Instantiate(craft_SelectedTab[selectedSlotNumber].go_PreviewPrefab, tf_Player.position + tf_Player.forward, Quaternion.identity);
+        go_Prefab = craft_SelectedTab[selectedSlotNumber].go_Prefab;
         isPreviewActivated = true;
         go_BaseUI.SetActive(false);
         GameManager.isOpenInventory = false;
